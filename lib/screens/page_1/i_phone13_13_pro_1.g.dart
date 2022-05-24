39f1da864b6103845e5373205215b397608bde89
Parabeac-Nest <admin@parabeac.com>
2022-05-24 01:20:29 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/screens/page_1/i_phone13_13_pro_1.g.dart
@@ -0,0 +1,465 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+class IPhone1313Pro1 extends StatefulWidget {
+  const IPhone1313Pro1({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _IPhone1313Pro1 createState() => _IPhone1313Pro1();
+}
+
+class _IPhone1313Pro1 extends State<IPhone1313Pro1> {
+  _IPhone1313Pro1();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Color(0xff1a1851),
+      child: Stack(children: [
+        Positioned(
+          left: 0,
+          width: 484.0,
+          top: 0,
+          height: 450.0,
+          child: Image.asset(
+            'assets/images/ellipse1.png',
+            package: 'app',
+            width: 484.000,
+            height: 450.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 92.0,
+          width: 301.0,
+          top: 295.0,
+          height: 316.0,
+          child: Container(
+            width: 301.000,
+            height: 316.000,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0x40000000),
+                  spreadRadius: 4,
+                  blurRadius: 4,
+                  offset: Offset(0, 4),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          left: 168.0,
+          width: 139.0,
+          top: 324.0,
+          height: 139.0,
+          child: Image.asset(
+            'assets/images/ellipse2.png',
+            package: 'app',
+            width: 139.000,
+            height: 139.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 175.0,
+          width: 127.0,
+          top: 483.0,
+          height: 24.0,
+          child: Container(
+              width: 127.000,
+              height: 24.000,
+              child: AutoSizeText(
+                'PENDRAGON',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Color(0xff1a1851),
+                  shadows: [
+                    Shadow(
+                      color: Color(0x40000000),
+                      offset: Offset(0, 4),
+                      blurRadius: 4,
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 203.0,
+          width: 72.0,
+          top: 511.0,
+          height: 22.0,
+          child: Container(
+              width: 72.000,
+              height: 22.000,
+              child: AutoSizeText(
+                'Arthuria',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Color(0xcc1a1851),
+                  shadows: [
+                    Shadow(
+                      color: Color(0x40000000),
+                      offset: Offset(0, 4),
+                      blurRadius: 4,
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 176.0,
+          width: 126.0,
+          top: 549.0,
+          height: 42.0,
+          child: Container(
+            width: 126.000,
+            height: 42.000,
+            decoration: BoxDecoration(
+              color: Color(0xfffcb316),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 208.0,
+          width: 56.0,
+          top: 559.0,
+          height: 22.0,
+          child: Container(
+              width: 56.000,
+              height: 22.000,
+              child: AutoSizeText(
+                'Online',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Color(0xff1a1851),
+                  shadows: [
+                    Shadow(
+                      color: Color(0x40000000),
+                      offset: Offset(0, 4),
+                      blurRadius: 4,
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 364.0,
+          width: 38.0,
+          top: 210.0,
+          height: 37.0,
+          child: Image.asset(
+            'assets/images/rectangle3.png',
+            package: 'app',
+            width: 38.000,
+            height: 37.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 92.0,
+          width: 75.0,
+          top: 681.0,
+          height: 25.0,
+          child: Container(
+              width: 75.000,
+              height: 25.000,
+              child: AutoSizeText(
+                'System',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 21,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 93.0,
+          width: 36.0,
+          top: 725.0,
+          height: 33.0,
+          child: Container(
+            width: 36.000,
+            height: 33.000,
+            decoration: BoxDecoration(
+              color: Color(0xffd9d9d9),
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 135.0,
+          width: 156.0,
+          top: 726.0,
+          height: 15.0,
+          child: Container(
+              width: 156.000,
+              height: 15.000,
+              child: AutoSizeText(
+                'Document Tracking System',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 135.0,
+          width: 20.0,
+          top: 746.0,
+          height: 12.0,
+          child: Container(
+              width: 20.000,
+              height: 12.000,
+              child: AutoSizeText(
+                'DTS',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 10,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 360.0,
+          width: 21.0,
+          top: 730.0,
+          height: 21.0,
+          child: Image.asset(
+            'assets/images/ellipse3.png',
+            package: 'app',
+            width: 21.000,
+            height: 21.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 93.0,
+          width: 36.0,
+          top: 725.0,
+          height: 33.0,
+          child: Container(
+            width: 36.000,
+            height: 33.000,
+            decoration: BoxDecoration(
+              color: Color(0xffd9d9d9),
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 135.0,
+          width: 156.0,
+          top: 726.0,
+          height: 15.0,
+          child: Container(
+              width: 156.000,
+              height: 15.000,
+              child: AutoSizeText(
+                'Document Tracking System',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 135.0,
+          width: 20.0,
+          top: 746.0,
+          height: 12.0,
+          child: Container(
+              width: 20.000,
+              height: 12.000,
+              child: AutoSizeText(
+                'DTS',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 10,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 93.0,
+          width: 36.0,
+          top: 782.0,
+          height: 33.0,
+          child: Container(
+            width: 36.000,
+            height: 33.000,
+            decoration: BoxDecoration(
+              color: Color(0xffd9d9d9),
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 135.0,
+          width: 210.0,
+          top: 783.0,
+          height: 15.0,
+          child: Container(
+              width: 210.000,
+              height: 15.000,
+              child: AutoSizeText(
+                'Human Resource Information System',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 135.0,
+          width: 23.0,
+          top: 803.0,
+          height: 12.0,
+          child: Container(
+              width: 23.000,
+              height: 12.000,
+              child: AutoSizeText(
+                'HRIS',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 10,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 360.0,
+          width: 21.0,
+          top: 787.0,
+          height: 21.0,
+          child: Image.asset(
+            'assets/images/ellipse4.png',
+            package: 'app',
+            width: 21.000,
+            height: 21.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 93.0,
+          width: 36.0,
+          top: 840.0,
+          height: 33.0,
+          child: Container(
+            width: 36.000,
+            height: 33.000,
+            decoration: BoxDecoration(
+              color: Color(0xffd9d9d9),
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 135.0,
+          width: 156.0,
+          top: 841.0,
+          height: 15.0,
+          child: Container(
+              width: 156.000,
+              height: 15.000,
+              child: AutoSizeText(
+                'Document Tracking System',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 135.0,
+          width: 20.0,
+          top: 861.0,
+          height: 12.0,
+          child: Container(
+              width: 20.000,
+              height: 12.000,
+              child: AutoSizeText(
+                'DTS',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 10,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 360.0,
+          width: 21.0,
+          top: 845.0,
+          height: 21.0,
+          child: Image.asset(
+            'assets/images/ellipse6.png',
+            package: 'app',
+            width: 21.000,
+            height: 21.000,
+            fit: BoxFit.none,
+          ),
+        ),
+      ]),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
